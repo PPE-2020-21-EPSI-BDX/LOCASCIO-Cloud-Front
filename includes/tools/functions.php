81f27ae7f7cd00000426bfa136c945763aa08e02
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-21 09:01:11 +0100

    Ajout de phpMyAdmin

--- includes/tools/functions.php
+++ includes/tools/functions.php
@@ -86,7 +86,11 @@ if (!function_exists('get_page')) {
                     $content = ob_get_clean();
                     break;
 
-
+                case '/phpMyAdmin':
+                    ob_start();
+                    include __REALPATH__ . '/assets/phpMyAdmin/index.php';
+                    $content = ob_get_clean();
+                    break;
                 case '/admin':
                     define('ADMIN', true);
                     ob_start();
